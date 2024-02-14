99fda80ee2fbde87ff730d9af79d253ca37dde43
danieloka <danirc01@gmail.com>
2024-02-14 12:23:20 -0500

    pagination fixes

--- pages/movies.js
+++ pages/movies.js
@@ -17,40 +17,50 @@ const options = {
             searchTitle: "",
             id: "",
             isFavorite: false,
+            currentPage: 1,
+            perPage: 16 // Number of movies per page
         }
     },
     beforeCreate() {
         fetch(url, optionsKey)
         .then(response => response.json())
-        .then( data  => {
+        .then(data  => {
             this.movies = data.movies
-            this.genres = [...new Set (this.movies.map(movie => movie.genres).flat().sort())]
+            this.genres = [...new Set(this.movies.map(movie => movie.genres).flat().sort())]
             this.filteredMovies = this.movies
             this.id = this.movies.find(movie => movie.id)
             this.isFavorite = JSON.parse(localStorage.getItem("favoriteMovies")) || []
-      
-            
         })
         .catch(err => console.log(err))
     },
     methods: {
         addFav(movieId) {
-            if(this.isFavorite.includes(movieId)) {
+            if (this.isFavorite.includes(movieId)) {
                 this.isFavorite.splice(this.isFavorite.indexOf(movieId), 1)
-            } else{
+            } else {
                 this.isFavorite.push(movieId)
             }
             localStorage.setItem("favoriteMovies", JSON.stringify(this.isFavorite))
-         
+        },
+        changePage(pageNumber) {
+            this.currentPage = pageNumber;
         }
-       
     },
     computed: {
+        totalPages() {
+            return Math.ceil(this.filteredMovies.length / this.perPage);
+        },
+        paginatedMovies() {
+            const startIndex = (this.currentPage - 1) * this.perPage;
+            const endIndex = startIndex + this.perPage;
+            return this.filteredMovies.slice(startIndex, endIndex);
+        },
         filterMovies() {
             const aux = this.movies.filter(movie => movie.title.toLowerCase().startsWith(this.searchTitle.toLowerCase()) && (this.selectedGenre == "all" || movie.genres.includes(this.selectedGenre)))
-            this.filteredMovies = aux
+            this.filteredMovies = aux;
         }
     }
 }
+
 const app = createApp(options)
 app.mount("#app")
